78b9b146c1d43aaf5ae17aa21a345a77171ca691
tianshuo <297497182@qq.com>
2026-05-16 15:49:36 +0800

    chore: 清除调试日志

--- src/main/stats-manager.ts
+++ src/main/stats-manager.ts
@@ -47,7 +47,6 @@ export class StatsManager {
 
   constructor(dataRoot: string) {
     this.statsPath = path.join(dataRoot, 'stats.jsonl')
-    console.log('[StatsManager] 初始化, statsPath:', this.statsPath)
   }
 
   /**
